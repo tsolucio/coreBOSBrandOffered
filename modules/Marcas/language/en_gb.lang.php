84370905f4372f4ac5449918c082cb93bdbe60d9
Joe Bordes <joe@tsolucio.com>
2015-01-10 01:02:06 +0100

    coreBOS Brand Offered module

---
+++ modules/Marcas/language/en_gb.lang.php
@@ -0,0 +1,31 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Marcas' => 'Brands',
+	'SINGLE_Marcas' => 'Brand',
+	'Marcas ID' => 'Brand ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_MARCAS_INFORMATION' => 'Brand Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+	'Description' => 'Description',
+
+	'marcasno' => 'Brand No',
+	'marcaname' => 'Brand',
+	'puedecomprar' => '¿Can buy?',
+	'compra' => '¿Bought?',
+	'presentado' => 'Presented',
+	'novedades' => 'Novelties',
+	'accid' => 'Account',
+	'ctoid' => 'Contact',
+);
+
+?>
---
+++ modules/Marcas/language/es_es.lang.php
@@ -0,0 +1,31 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:^tvtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Marcas' => 'Marcas',
+	'SINGLE_Marcas' => 'Marca',
+	'Marcas ID' => 'ID Marca',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_MARCAS_INFORMATION' => 'Información Marca',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+	'Description' => 'Descripción',
+
+	'marcasno' => 'Nº Marca',
+	'marcaname' => 'Marca',
+	'puedecomprar' => '¿Puede comprar?',
+	'compra' => '¿Compra?',
+	'presentado' => 'Presentado',
+	'novedades' => 'Novedades',
+	'accid' => 'Cuenta',
+	'ctoid' => 'Contacto',
+);
+
+?>
